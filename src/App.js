e9cf9df801ce3f5f0a4656f8d4cdf2863eb95786
tamer-fs <113843535+tamer-fs@users.noreply.github.com>
2024-06-01 11:33:39 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = (e) => {
+    e.preventDefault();
+    setCount(count + 1);
+  };
+
+  const decrement = (e) => {
+    e.preventDefault();
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{count}</p>
+      <button onClick={(e) => decrement(e)}>decrement</button>
+      <button onClick={(e) => increment(e)}>increment</button>
+      <p>hello</p>
+    </>
   );
 }
 
